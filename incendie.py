dae305834a07459d7e96060659597d1accbe10d2
Maxime DANNEVILLE <maxime.danneville@ens.uvsq.fr>
2021-02-17 10:19:06 +0100

    debut

--- incendie.py
+++ incendie.py
@@ -1,5 +1,8 @@
-print("oui")
-
-print("Non")
-
-import tkinter as
+#########################################
+# groupe MPCI 3
+# Lalasoa RATOVONDRANTO
+# Ahmadou Bamba SOIM
+# Maxime DANEVILLE
+# https://github.com/uvsq22000372/projet_incendie
+#########################################
+import tkinter as tk
