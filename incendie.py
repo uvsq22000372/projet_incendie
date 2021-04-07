d79508916574e0e4398960a0ac7b8070e92cacb8
Elias SAUVAGE <elias.sauvage@ens.uvsq.fr>
2021-04-07 17:29:02 +0200

    passage de l'incendie au Projet 2 (besoin de débugger l'indexage lors de la boucle for dans random_terrain() )

--- incendie.py
+++ incendie.py
@@ -16,32 +16,34 @@ import random as rd
 
 COUL_FOND = "grey20"
 COULEUR_QUADR = "grey60"
-LARGEUR = 800
-HAUTEUR = 800
+LARGEUR = 1000
+HAUTEUR = 1000
 COTE = 20
-DUREE_FEU = 4000 # en ms
-DUREE_CENDRES = 10000 # en ms
+n = 4
+x = 0
 
 #definition variables globales 
 nombre_eau = []
-nombre_prairie = []
-nombre_foret = []
+nombre_terre = []
+l_carré = []
 
-Couleur = ["dodger blue", "dodger blue", "green4", "green4", "green4", "green4", "green4", "yellow","yellow", "yellow" ]
+coul_eau = ["RoyalBlue1"]
+coul_terre = ["salmon4"]
 
 
 #definition des fonctions
 def Carré():
     x0, y0, x1, y1 = 0, 0, COTE, COTE
-    while y0 <= HAUTEUR and x1 <= LARGEUR:
-        i = rd.randint(0, 9)
-        carré = canvas.create_rectangle(x0, y0, x1, y1,fill=Couleur[i])
-        if i >= 0 and i < 2:
-            nombre_eau.append(carré)
-        elif i >= 2 and i < 6:
-            nombre_foret.append(carré)
-        elif i >= 6 and i < 10:
-            nombre_prairie.append(carré)
+    while y0 < HAUTEUR and x1 <= LARGEUR:
+        carré = canvas.create_rectangle(x0, y0, x1, y1,fill=coul_eau[0])
+        l_carré.append(carré)
+        nombre_eau.append(carré)
+        #if i >= 0 and i < 2:
+        #   nombre_eau.append(carré)
+        #elif i >= 2 and i < 6:
+        #    nombre_foret.append(carré)
+        #elif i >= 6 and i < 10:
+        #    nombre_prairie.append(carré)
         x0 += COTE
         x1 += COTE
         if x0 == LARGEUR:
@@ -49,13 +51,28 @@ def Carré():
             x1 = COTE 
             y0 += COTE
             y1 += COTE
-    print(nombre_eau)
-    print(nombre_foret)
-    print(nombre_prairie)
+    print(l_carré)
+
         
 
 def random_terrain():
-    pass
+    global nombre_eau
+    global nombre_terre
+    global x
+    for i in (0, n):
+        for i in l_carré:
+            r = rd.randint(0, 1)
+            if r == 0:
+                pass
+            else:
+                canvas.itemconfig((l_carré[i]) , fill=coul_terre[0])
+                nombre_terre.append(l_carré[i])
+                del nombre_eau[i - x]
+                x += 1
+        print(nombre_eau)
+        print(nombre_terre)
+
+
 def GetColor(r, g, b):
     """ Retourne une couleur à partir de ses composantes r, g, b entre 0 et 255"""
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
@@ -82,6 +99,7 @@ canvas.bind("<Button-1>")
 
 # autres fonctions
 Carré()
+random_terrain()
 
 
 # boucle principal
